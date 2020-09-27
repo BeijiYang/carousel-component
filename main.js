687bd37c8c8ff92fbdcfe1def4669c73f8282b38
BeijiYang <langyuyang@outlook.com>
2020-09-27 15:22:40 +0200

    add createElement fn

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
-for (const iterator of [1, 2, 3]) {
-  console.log(iterator)
+function createElement(type, attributes, ...children) {
+  const element = typeof type === 'string'
+    ? document.createElement(type)
+    : new type;
+
+  for (const attr in attributes) {
+    element.setAttribute(attr, attributes[attr]);
+  }
+
+  for (let child of children) {
+    if (typeof child === 'string') {
+      child = document.createTextNode(child);
+    }
+    element.appendChild(child);
+  }
+
+  return element;
 }
 
-const a = <div />
+
+
+const a = <div id="aa">
+  <span>Hey</span>
+  ~
+</div>
+
+document.body.appendChild(a);
+
+
+// var a = createElement("div", {
+//     id: "aa"
+//   },
+//   createElement("span", null),
+//   createElement("span", null),
+//   createElement("span", null)
+// );
