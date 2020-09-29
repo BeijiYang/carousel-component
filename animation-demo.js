5720c6d088519ba9a95a4e81b378fc1feb787f81
BeijiYang <langyuyang@outlook.com>
2020-09-29 19:20:02 +0200

    add cubicBezier function & demo

--- animation-demo.js
+++ animation-demo.js
@@ -1,4 +1,5 @@
 import { Timeline, Animation } from './animation';
+import { ease, easeIn } from './ease';
 
 const timeline = new Timeline();
 
@@ -11,10 +12,15 @@ timeline.add(new Animation(
   0,
   500,
   2000,
-  1000,
-  null,
+  0,
+  // null,
+  ease,
   val => `translateX(${val}px)`,
 ));
+
+// document.querySelector('.test').style.transition = 'transform easeIn 2s';
+document.querySelector('.test').style.transform = 'translateX(500px)';
+
 
 document.querySelector('.pause-btn').addEventListener('click', timeline.pause.bind(timeline));
 document.querySelector('.resume-btn').addEventListener('click', timeline.resume.bind(timeline));
